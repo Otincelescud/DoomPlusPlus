d9b8f4182a151f97f9579913dc42ff206f58d624
Otincelescu Darius Marian <otincelescud@gmail.com>
2024-11-03 17:31:30 +0200

    Start Writing Vector Class

---
+++ src/vectorN.h
@@ -0,0 +1,16 @@
+#ifndef VectorN_h
+#define VecotrN_h
+
+#include <array>
+#include <cmath>
+
+template<int N> class VectorN {
+public:
+	
+
+
+private:
+	array<N> values;
+};
+
+#endif /* VectorN_h */
